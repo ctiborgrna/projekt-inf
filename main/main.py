63e1f79dd0653a22bf0edc5eda8ce7c8a84eb49b
ctiborgrna <meteorx147@gmail.com>
2025-09-30 15:17:40 +0200

    okno

--- main/main.py
+++ main/main.py
@@ -1,15 +1,19 @@
-import tkinter
+from tkinter import *
+window=Tk()
+window.geometry('{}x{}+0+0'.format(window.winfo_screenwidth(), window.winfo_screenheight()))
+window.title("Logic Gates Simulator")
+window.configure(bg='white')
+
 import webbrowser
+canvas = Canvas(window, width=window.winfo_screenwidth(), height=window.winfo_screenheight(), bg='white')
+canvas.pack()
 
 def open_link(event):
     webbrowser.open_new("https://www.101computing.net/logic-gates-studio/")
 
-canvas = tkinter.Canvas(width=400, height=400, bg='white')
-canvas.pack()
-
-link_text = canvas.create_text(200, 200, text='https://www.101computing.net/logic-gates-studio/', font=('Arial', 12), fill='blue')
+link_text = canvas.create_text(window.winfo_screenwidth()/2, window.winfo_screenheight()/2, text='SIMULATOR', font=('Arial', 64), fill='blue')
 canvas.tag_bind(link_text, "<Button-1>", open_link)
 
 canvas.create_oval(50, 150, 350, 250, outline='red', width=5)
-####test3339
-tkinter.mainloop()
+
+window.mainloop()
